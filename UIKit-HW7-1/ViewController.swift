a8669f1cd86992ab3779e6c39cb96803b707c51b
Varyzgin <dima5varyzgin@gmail.com>
2024-12-05 11:35:44 +0300

    Files add

---
+++ UIKit-HW7-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-HW7-1
+//
+//  Created by Дима on 12/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
